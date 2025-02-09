a87ab1a3ecbebda1bd90f8f2448aa3a9772bfc61
Niklas P <contact@niklasplessing.net>
2025-02-09 16:31:30 +0100

    alignment

--- components/income-impact.tsx
+++ components/income-impact.tsx
@@ -280,7 +280,7 @@ export default function IncomeImpact() {
   );
 
   return (
-    <div className="container mx-auto px-4 py-12 sm:px-6 max-w-3xl justify-items-center">
+    <div className="container mx-auto px-4 py-12 sm:px-6 max-w-3xl ">
       <h1 className="text-2xl font-bold mb-6 sm:mb-8">
         Auswirkungen der Bundestagswahl am 23.02.2025 auf Ihr Einkommen
       </h1>
